fc886af3411390ce51b516a730453389c5e989b1
MGlaJ <93045457+MGlaJ@users.noreply.github.com>
2021-10-23 18:09:23 +0200

    Add files via upload

---
+++ wyk 1,2,3/rozdzial2cwiczenie1/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(imie,nazwisko)
+{
+    printf("Marta Gla\n");
+    printf("Marta\n"
+           "Gla\n");
+    printf("Marta\n");
+    printf("Gla\n");
+    return 0;
+}
---
+++ wyk 1,2,3/rozdzial2cwiczenie3/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    int A,365,iloczyn;
+
+
+    printf("Ile masz lat?\n");
+    scanf("d%,&A");
+    printf("Wiec masz %d dni\n", iloczyn=A*(365);
+    return 0;
+}
